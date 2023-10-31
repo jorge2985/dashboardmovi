bd968c26849856ada88efe7b885e6d8c97d51872
jorge2985 <37635318+jorge2985@users.noreply.github.com>
2023-10-31 20:03:20 -0300

    Se agregan charts

---
+++ app.js
@@ -0,0 +1,14 @@
+import * as echarts from 'https://cdn.jsdelivr.net/npm/echarts@5.4.3/dist/echarts.min.js';
+
+import './charts/facebook/facebook.js';
+import './charts/whatsapp/whatsapp.js';
+import './charts/facebook/facebookCasos.js';
+import './charts/facebook/facebookTPRC.js';
+import './charts/facebook/facebookTC.js';
+import './charts/facebook/facebookDem.js';
+import './charts/casos.js';
+import './charts/tprc.js';
+import './charts//productividad/productividad.js'
+import './charts/whatsapp/whatsappTC.js';
+import './charts/whatsapp/whatsappDem.js';
+import './charts/whatsapp/whatsappTPRC.js';
